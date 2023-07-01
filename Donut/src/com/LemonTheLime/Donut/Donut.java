c7425cffc7370d4003ae99eebf54b575c26a247c
bcd54 <bcd54@10.0.0.80>
2023-07-01 17:30:16 -0500

    Donut projected onto screen. Illumination still needs work.

--- Donut/src/com/LemonTheLime/Donut/Donut.java
+++ Donut/src/com/LemonTheLime/Donut/Donut.java
@@ -13,41 +13,48 @@ import javax.swing.JFrame;
 
 
 
-
+/* Donut
+ * This program renders a pixelated donut on screen, inspired by the donut.c project.
+ * The program is displayed through a 640 x 360 jframe.
+ * The program uses a directional light source. It may potentially be extended to a customizable point or spotlight source.
+ */
 public class Donut extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	//STATIC FIELDS
-	public static final int SCALE = 8; //80 x 45 resolution
-	public static final int WIDTH = 2 * 320 / SCALE; //screen's pixel resolution
-	public static final int HEIGHT = 2 * 180 / SCALE; //screen's pixel resolution
+	public static final int WIDTH = 16; //width of the virtual screen
+	public static final int HEIGHT = 9; //height of the virtual screen
+	public static final int SCALE = 4; //160 x 90 resolution
+	public static final int RES_X = 640 / SCALE; //screen's pixel resolution
+	public static final int RES_Y = 360 / SCALE; //screen's pixel resolution
 	public static String title = "Donut";
 	
 	//FRAME & THREAD FIELDS
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
-	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private BufferedImage image = new BufferedImage(RES_X, RES_Y, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData(); //final screen pixels
-	private int[] prePixels = new int[WIDTH * HEIGHT]; //array to hold pixels before drawing
+	private int[] prePixels = new int[RES_X * RES_Y]; //array to hold pixels before drawing
 	
 	//LOGIC FIELDS
 	private ArrayList<Point3D>points = new ArrayList<Point3D>();
 	private ArrayList<Point2D>plane;
 	private double focalLength;
 	private double cameraDistance;
-	private double[] zBuffer = new double[WIDTH * HEIGHT];
+	private double[] zBuffer = new double[RES_X * RES_Y];
+	private Vector3D lightVector = new Vector3D(0, -1, 0);
 	
 	//CONSTRUCTOR
 	public Donut() {
 		//create the frame and set the preferred dimensions
 		frame = new JFrame();
-		Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+		Dimension size = new Dimension(RES_X * SCALE, RES_Y * SCALE);
 		setPreferredSize(size);
 		
 		//set the camera settings (focalLength, cameraDistance)
 		focalLength = 10;
-		cameraDistance = 80;
+		cameraDistance = 30;
 		
 		//create the donut
 		createDonutPoints();
@@ -55,11 +62,13 @@ public class Donut extends Canvas implements Runnable {
 		
 	//create the all the points in the donut
 	private void createDonutPoints() {
-		double r1 = 24;
-		double r2 = 48;
-		int n1 = 50; //to be tested
-		int n2 = 50; //to be tested
+		double r1 = 3;
+		double r2 = 6;
+		int n1 = 150; //to be tested
+		int n2 = 200; //to be tested
 		
+		
+		//donut shell
 		for(int i = 0; i < n1; i++) {
 			double m = r2 + r1 * Math.cos(i * 2 * Math.PI / n1);
 			double x;
@@ -68,9 +77,16 @@ public class Donut extends Canvas implements Runnable {
 			for(int j = 0; j < n2; j++) {
 				x = m * Math.cos(j * 2 * Math.PI / n2);
 				z = m * Math.sin(j * 2 * Math.PI / n2);
-				points.add(new Point3D(x, y, z));
+				Point3D nextPoint = new Point3D(x, y, z);
+				nextPoint.setRingCenter(r2 * Math.cos(j * 2 * Math.PI / n2),
+						0, r2 * Math.sin(j * 2 * Math.PI / n2));
+				points.add(nextPoint);
 			}
 		}
+		//test
+		rotate(0, 0, 0);
+
+		return;
 	}
 	
 	//start the thread
@@ -109,12 +125,12 @@ public class Donut extends Canvas implements Runnable {
 	/* Update donut data
 	 * Rotate all the points
 	 */
-	public void update() {
-		
+	private void update() {
+		rotate(0.01, 0, 0);
 	}
 	
 	//render and draw the frame buffer
-	public void render() {
+	private void render() {
 		//create buffer strategy
 		BufferStrategy b = getBufferStrategy();
 		if(b == null) {
@@ -156,7 +172,7 @@ public class Donut extends Canvas implements Runnable {
 	}
 
 	//Project all the donut points on a xy plane. Fill the plane list with the points and use z-buffers to check
-	public void projectToScreen() {
+	private void projectToScreen() {
 		plane = new ArrayList<Point2D>();
 		for(int i = 0; i < points.size(); i++) {
 			plane.add(new Point2D(points.get(i)));
@@ -164,21 +180,38 @@ public class Donut extends Canvas implements Runnable {
 	}
 	
 	//render the plane list of 2d points to the screen and use z-buffers
-	public void renderPrePixels() {
-		int centerXOffset = (int)(WIDTH / 2);
-		int centerYOffset = (int)(HEIGHT / 2);
+	private void renderPrePixels() {
+		int centerXOffset = (int)(RES_X / 2);
+		int centerYOffset = (int)(RES_Y / 2);
+		int luminance;
 		for(int i = 0; i < plane.size(); i++) {
 			//code
-			int x = plane.get(i).getX() + centerXOffset;
-			int y = plane.get(i).getY() + centerYOffset;
-			if(zBuffer[x + y * WIDTH] == 0) {
+			int x = plane.get(i).getScreenX() + centerXOffset;
+			int y = plane.get(i).getScreenY() + centerYOffset;
+			//GET ILLUMINATION HERE
+			points.get(i).updateSurfaceNormal();
+			luminance = 0xffffff; //points.get(i).getLuminance();
+			luminance = points.get(i).getLuminance();
+			
+			//System.out.println("x: " + x + " | y: " + y + " | test: " + (x + y * RES_X));
+			if(zBuffer[x + y * RES_X] == 0) {
 				//initial rendering
-				prePixels[x + y * WIDTH] = 0xffffff;
-			} else if(plane.get(i).getZDepth() > zBuffer[x + y * WIDTH]) {
+				prePixels[x + y * RES_X] = luminance;
+			} else if(plane.get(i).getZDepth() > zBuffer[x + y * RES_X]) {
 				//greater zDepth
-				prePixels[x + y * WIDTH] = 0xffffff;
-				zBuffer[x + y * WIDTH] = plane.get(i).getZDepth();
+				prePixels[x + y * RES_X] = luminance;
+				zBuffer[x + y * RES_X] = plane.get(i).getZDepth();
 			}
+		}
+	}
+	
+	//rotate the entire donut
+	private void rotate(double x, double y, double z) {
+		for(int i = 0; i < points.size(); i++) {
+			points.get(i).rotateX(x);
+			points.get(i).rotateY(y);
+			points.get(i).rotateZ(z);
+
 		}
 	}
 	
@@ -201,6 +234,7 @@ public class Donut extends Canvas implements Runnable {
 	private class Point3D {
 		//FIELDS
 		private double x, y, z;
+		private double xr, yr, zr; //ring center point
 		private Vector3D surfaceNormal;
 		
 		//CONSTRUCTOR
@@ -210,6 +244,7 @@ public class Donut extends Canvas implements Runnable {
 			this.z = z;
 		}
 		
+
 		//get x
 		public double getX() {
 			return x;
@@ -224,19 +259,65 @@ public class Donut extends Canvas implements Runnable {
 			return z;
 		}
 		
+		//set ring center
+		private void setRingCenter(double xr, double yr, double zr) {
+			this.xr = xr;
+			this.yr = yr;
+			this.zr = zr;
+		}
+		
+		//set the surfaceNormal vector
+		private void updateSurfaceNormal() {
+			surfaceNormal = new Vector3D(x - xr, y - yr, z - zr);
+		}
+		
+		//get the hexadecimal grey luminance
+		public int getLuminance() {
+			//dot product between 0 - 1
+			double product = surfaceNormal.x * lightVector.x +
+					surfaceNormal.y * lightVector.y +
+					surfaceNormal.z * lightVector.z;
+			return getHexColor(product);
+		}
+		
+		//get the hex color from a double -1 to 1
+		private int getHexColor(double product) {
+			String v = Integer.toHexString((int)((product + 1) * 128));
+			if(v.length() == 1) {
+				v = "0" + v;
+			}
+			v = v + v + v;
+			try {
+				int finalValue = Integer.parseInt(v, 16);
+				return finalValue;
+			} catch(Exception e) {
+				System.out.println("v: " + v);
+			}
+			return 0;
+		}
+
+
 		//rotate x axis about origin (z, y)
 		public void rotateX(double a) {
 			double u = z;
 			double v = y;
 			z = u * Math.cos(a) - v * Math.sin(a);
 			y = u * Math.sin(a) + v * Math.cos(a);
+			u = zr;
+			v = yr;
+			zr = u * Math.cos(a) - v * Math.sin(a);
+			yr = u * Math.sin(a) + v * Math.cos(a);
 		}
 		//rotate y axis about origin (x, z)
 		public void rotateY(double a) {
 			double u = x;
 			double v = z;
-			x = x * Math.cos(a) - z * Math.sin(a);
-			z = x * Math.sin(a) + z * Math.cos(a);
+			x = u * Math.cos(a) - v * Math.sin(a);
+			z = u * Math.sin(a) + v * Math.cos(a);
+			u = xr;
+			v = zr;
+			xr = u * Math.cos(a) - v * Math.sin(a);
+			zr = u * Math.sin(a) + v * Math.cos(a);
 		}
 		//rotate z axis about origin (x, y)
 		public void rotateZ(double a) {
@@ -244,30 +325,33 @@ public class Donut extends Canvas implements Runnable {
 			double v = y;
 			x = u * Math.cos(a) - v * Math.sin(a);
 			y = u * Math.sin(a) + v * Math.cos(a);
+			u = xr;
+			v = yr;
+			xr = u * Math.cos(a) - v * Math.sin(a);
+			yr = u * Math.sin(a) + v * Math.cos(a);
 		}
 	}
 	
 	//PRIVATE 2D POINT CLASS
 	private class Point2D {
 		//FIELDS
-		private int x, y;
-		private double luminance;
+		private double x, y; //(x, y) on the virtual screen
 		private double zDepth;
 		
 		//CONSTRUCTOR
 		public Point2D(Point3D p) {
-			x = (int)(focalLength * p.getX() / (cameraDistance + p.getZ()));
-			y = (int)(focalLength * p.getY() / (cameraDistance + p.getZ()));
+			x = focalLength * p.getX() / (cameraDistance + p.getZ());
+			y = focalLength * p.getY() / (cameraDistance + p.getZ());
 			zDepth = 1 / (p.getZ() + cameraDistance);
 		}
 		
 		//get x
-		public int getX() {
-			return x;
+		public int getScreenX() {
+			return (int)(x * RES_X / WIDTH);
 		}
 		//get y
-		public int getY() {
-			return y;
+		public int getScreenY() {
+			return (int)(y * RES_Y / HEIGHT);
 		}
 		//get zDepth
 		public double getZDepth() {
@@ -283,9 +367,10 @@ public class Donut extends Canvas implements Runnable {
 		
 		//CONSTRUCTOR
 		public Vector3D(double x, double y, double z) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
+			double s = Math.sqrt(x * x + y * y + z * z);
+			this.x = x / s;
+			this.y = y / s;
+			this.z = z / s;
 		}
 	}
 
